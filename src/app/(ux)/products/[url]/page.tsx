eb3b11d59dd160c2287193713998ef23f5df983a
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2025-06-24 15:18:38 +0300

    2025-06-24-15-18

--- src/app/(ux)/products/[url]/page.tsx
+++ src/app/(ux)/products/[url]/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { getProductByUrlAndLang } from '@/features/product/api/productService';
 import { Product } from '@/features/product/types/product';
-import { useParams } from 'next/navigation';
 import React, { useEffect } from 'react'
+import { Container } from 'react-bootstrap';
 
 export default function productDetail({ params }: { params: { locale: string, slug: string } }) {
     const { locale, slug } = params;
@@ -17,6 +17,15 @@ export default function productDetail({ params }: { params: { locale: string, sl
     return (
         <>
             <h1>{product?.code}</h1>
+            <Container>
+                {product ? product.productTranslations.map((translation) => (
+                    <div key={translation.langCode}>
+                        <h2>{translation.brief}</h2>
+                        <p>{translation.brief}</p>
+                        <p>{translation.content}</p>
+                    </div>
+                )) : null}
+            </Container>
         </>
     )
 }
